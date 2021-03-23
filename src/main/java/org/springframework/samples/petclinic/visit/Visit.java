536490042263e67a40e39ba8a1fad72b0a3197c3
itamarweb <46376023+itamarweb@users.noreply.github.com>
2021-03-23 10:47:29 +0200

    test

--- src/main/java/org/springframework/samples/petclinic/visit/Visit.java
+++ src/main/java/org/springframework/samples/petclinic/visit/Visit.java
@@ -35,6 +35,10 @@ import org.springframework.samples.petclinic.model.BaseEntity;
 @Table(name = "visits")
 public class Visit extends BaseEntity {
 
+
+	int x = 6;
+	
+
 	@Column(name = "visit_date")
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate date;
